ba926602a81544362316109cf80a30b86fc25332
Julian Stradolini <julianstradolini@gmail.com>
2022-12-05 01:03:28 -0300

    estructura2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './components/main-body/register-form/register-form.component';
+import { LoginFormComponent } from './components/main-body/login-form/login-form.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainBodyComponent } from './components/main-body/main-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterFormComponent,
     LoginFormComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainBodyComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/main-body/register-form/register-form.component.ts
+++ src/app/components/main-body/register-form/register-form.component.ts
@@ -23,3 +23,5 @@ export class RegisterFormComponent implements OnInit {
     
   }
 }
+
+//editar por aca!
